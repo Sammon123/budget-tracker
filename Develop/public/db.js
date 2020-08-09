4d9fca5e9ea9d7e6ac571bf0fa91d6e9913165ec
Peter Sammon <petersammon2@gmail.com>
2020-08-09 10:20:51 -0400

    psuedocode db.js

---
+++ Develop/public/db.js
@@ -0,0 +1,3 @@
+// check app is online before reading db
+// delete records if successful
+// listen for app coming back online
--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,3 +1,6 @@
 // perform the install steps
 // cache all requests to the api routes
 // if successful clone the response and store it in the cache
+// catch the network request if it has failed
+// try to get it from the cache
+// return the cached home pages for all requests for the html pages
